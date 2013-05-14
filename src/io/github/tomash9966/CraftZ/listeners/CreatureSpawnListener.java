d8dbddf64a2d36eb7e898acb46c7085f3fdecb90
Tomash9966 <tomash9966@gmail.com>
2013-05-14 17:12:45 +0200

    Rewrited faster zombies, now they doesn't use speed potion.

--- src/io/github/tomash9966/CraftZ/listeners/CreatureSpawnListener.java
+++ src/io/github/tomash9966/CraftZ/listeners/CreatureSpawnListener.java
@@ -1,17 +1,32 @@
 package io.github.tomash9966.CraftZ.listeners;
 
+import java.lang.reflect.Field;
+
 import io.github.tomash9966.CraftZ.CraftZPlugin;
 
-import org.bukkit.Bukkit;
+import net.minecraft.server.v1_5_R3.EntityHuman;
+import net.minecraft.server.v1_5_R3.EntityLiving;
+import net.minecraft.server.v1_5_R3.EntityVillager;
+import net.minecraft.server.v1_5_R3.EntityZombie;
+import net.minecraft.server.v1_5_R3.PathfinderGoalBreakDoor;
+import net.minecraft.server.v1_5_R3.PathfinderGoalFloat;
+import net.minecraft.server.v1_5_R3.PathfinderGoalLookAtPlayer;
+import net.minecraft.server.v1_5_R3.PathfinderGoalMeleeAttack;
+import net.minecraft.server.v1_5_R3.PathfinderGoalMoveThroughVillage;
+import net.minecraft.server.v1_5_R3.PathfinderGoalMoveTowardsRestriction;
+import net.minecraft.server.v1_5_R3.PathfinderGoalRandomLookaround;
+import net.minecraft.server.v1_5_R3.PathfinderGoalRandomStroll;
+import net.minecraft.server.v1_5_R3.PathfinderGoalSelector;
+
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_5_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_5_R3.entity.CraftZombie;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class CreatureSpawnListener implements Listener{
 
@@ -30,22 +45,59 @@ public class CreatureSpawnListener implements Listener{
 
 		if(entitytype.equals(EntityType.ZOMBIE)){
 
-			final LivingEntity entity = event.getEntity();
+			EntityZombie zombie = ((CraftZombie) event.getEntity()).getHandle();
+	        Field fGoalSelector;
+	        //Field fdamage;
 
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(this.plugin, new Runnable(){
+			try {
+				fGoalSelector = EntityLiving.class.getDeclaredField("goalSelector");
+		        fGoalSelector.setAccessible(true);
+		        Float speed = 0.5F;
 
-				@Override
-				public void run() {
+		        PathfinderGoalSelector gs = new PathfinderGoalSelector(
 
-					entity.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 2));
+		        	((CraftWorld) event.getEntity().getWorld()).getHandle() != null &&
+		        	((CraftWorld) event.getEntity().getWorld()).getHandle().methodProfiler != null ?
+		        	((CraftWorld) event.getEntity().getWorld()).getHandle().methodProfiler : null);
 
-					entity.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1200, 2));
+		        	gs.a(0, new PathfinderGoalFloat(zombie));
+		        	gs.a(1, new PathfinderGoalBreakDoor(zombie));
+		        	gs.a(2, new PathfinderGoalMeleeAttack(zombie, EntityHuman.class, speed, false));
+		        	gs.a(3, new PathfinderGoalMeleeAttack(zombie, EntityVillager.class, speed, true));
+		        	gs.a(4, new PathfinderGoalMoveTowardsRestriction(zombie, speed));
+		        	gs.a(5, new PathfinderGoalMoveThroughVillage(zombie, speed, false));
+		        	gs.a(6, new PathfinderGoalRandomStroll(zombie, speed));
+		        	gs.a(7, new PathfinderGoalLookAtPlayer(zombie, EntityHuman.class, 15.0F));
+		        	gs.a(7, new PathfinderGoalRandomLookaround(zombie));
+		        	fGoalSelector.set(zombie, gs);
 
-				}
+			}
+			catch (NumberFormatException e){
 
+				e.printStackTrace();
 
+			}
+			catch (SecurityException e) {
 
-			},0L, 1000L);
+				e.printStackTrace();
+
+			}
+			catch (NoSuchFieldException e) {
+
+				e.printStackTrace();
+
+			}
+			catch (IllegalArgumentException e) {
+
+				e.printStackTrace();
+
+			}catch (IllegalAccessException e) {
+
+				e.printStackTrace();
+
+			}
+
+			LivingEntity entity = event.getEntity();
 
 			entity.setMaxHealth(50);
 
